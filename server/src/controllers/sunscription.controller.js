d44e3816ca35b308f66190f75ffab198526f82db
Vanshika Chhindre <chhindrevanshika@gmail.com>
2024-08-31 12:14:52 +0530

    completed backend

--- server/src/controllers/sunscription.controller.js
+++ server/src/controllers/sunscription.controller.js
@@ -32,12 +32,91 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if(!channelId || isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const pipeline = [
+        {$match : {channel : channelId}},
+
+        {
+            $lookup : {
+                from : "users",
+                localField : "subscriber",
+                foreignField : "_id",
+                as : "subscriberDetails"
+            }
+        },
+
+        {
+            $unwind : "$subscriberDetails"
+        },
+        {
+            $project : {
+                _id : 0,
+                subscriberId: "$subscriberDetails._id", 
+                username: "$subscriberDetails.username", 
+                avatar: "$subscriberDetails.avatar" 
+            }
+        }
+
+    ]
+
+    const subscriberList = await Subscription.aggregate(pipeline)
+   
+    if(!subscriberList){
+        throw new ApiError(500, "Could not fetch subscribers!")
+    }
+    
+
+    res.status(200).json(200, subscriberList, "Subscribers fetched successfully!")
     
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!subscriberId || isValidObjectId( subscriberId)){
+        throw new ApiError(400, "Invalid subscriber id")
+    }
+
+    const pipeline = [
+        {$match : {subscriberId :  mongoose.Types.ObjectId(subscriberId) }},
+
+        {
+            $lookup : {
+                from : "users",
+                localField : "channel",
+                foreignField : "_id",
+                as : "channelDetails"
+            }
+        },
+
+        {
+            $unwind : "$channelDetails"
+        },
+        {
+            $project : {
+                _id : 0,
+                channelId: "$channelDetails._id", 
+                username: "$channelDetails.username", 
+                avatar: "$channelDetails.avatar" 
+            }
+        }
+
+    ]
+
+    const channelList = await Subscription.aggregate(pipeline)
+   
+    if(!channelList){
+        throw new ApiError(500, "Could not fetch channels!")
+    }
+    
+
+    res.status(200).json(200, subscriberList, "Channels fetched successfully!")
+    
 })
 
 export {
